92b4030939e439022e6b45dd89031cf539259219
tomthe <tomthe@localhost>
2014-05-17 12:57:40 +0200

    zeigt indented text mit knotentexten an, inklusive refs und funktionsaufruf der noch nichts macht. etwas umstrukturiert. todo:  * knoten einklappen/ausklappen    * durch klick    * durch zus?tzliches attribut "FOLDED"    * anfangs alle zugeklappt  * knoten durch doppelklick bearbeiten  * map abspeichern

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from kivy.uix.modalview import ModalView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ListProperty, StringProperty,ObjectProperty
 from kivy.app import App
-from readmm import stringize
+#from readmm import stringize
+from lxml import etree
+
 
 kivy.require('1.0.7')
 
@@ -16,9 +18,38 @@ class ColorView(ModalView):
     
 class MainView(BoxLayout):
     mml = ObjectProperty()
+    raw_label_text = ""
+    
+    
+    def read_map_from_file(self,filename):
+        tree = etree.parse(filename)
+        self.rootnode = tree.getroot()
+        self.raw_label_text = ""
+        self.raw_label_text = self.parse_node_to_text(self.rootnode.find("node"),0,"")
+        return self.raw_label_text
+        
+    def parse_node_to_text(self,node,level,outstr):    
+        if node.tag=="node":
+            #print " " * level, level, node.get("TEXT"), node.tag
+            outstr += "    " * level + "[ref=" + node.get("ID") + "]" +  node.get("TEXT") +"[/ref]"+ chr(10)
+            for nodechild in node:
+                if nodechild.tag=="node":
+                    #print "gugu1, ",nodechild.tag
+                    outstr = self.parse_node_to_text(nodechild,level+1,outstr)
+        else:
+            print "what?", node.tag
+                
+        return outstr    
+    
+    def nodepress(self,instance,value):
+        print "press , ", value
     
     def filllabel(self):
-       self.mml.text = stringize("testtemp.mm")
+        print "bla"
+        self.mml.text = self.read_map_from_file("testtemp.mm")
+        self.mml.bind(on_ref_press=self.nodepress)
+ #       self.mml.on_ref_press(self.nodepress)
+        self.mml.markup=True
 
 class mmviewApp(App):
 
