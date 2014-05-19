471a5134b56af92e8889b958e77d85ff49ec1d2c
Tom Theile <tomthe@users.noreply.github.com>
2014-05-19 08:12:44 +0200

    displaying, scrolling and folding works (as one big label)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import kivy
 from kivy.uix.modalview import ModalView
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ListProperty, StringProperty,ObjectProperty
 from kivy.app import App
 #from readmm import stringize
@@ -16,7 +17,7 @@ class ColorView(ModalView):
     answer_text = StringProperty('')
     font_size = StringProperty('50dp')
     
-class MainView(BoxLayout):
+class MainView(FloatLayout):
     mml = ObjectProperty()
     raw_label_text = ""
     
@@ -31,10 +32,10 @@ class MainView(BoxLayout):
         
     def parse_node_to_text(self,node,level,outstr):    
         if node.tag=="node":
-        
-            print " " * level, level, node.get("TEXT"), node.tag
+            #print " " * level, level, node.get("TEXT"), node.tag
             outstr += "    " * level + "[ref=" + node.get("ID") + "]" +  node.get("TEXT") +"[/ref]"+ chr(10)            
             if node.get("FOLDED",default="False")=="False":
+                node.set("FOLDED","False")
                 for nodechild in node:
                     if nodechild.tag=="node":
                         #print "gugu1, ",nodechild.tag
@@ -42,13 +43,12 @@ class MainView(BoxLayout):
                             outstr = self.parse_node_to_text(nodechild,level+1,outstr)
         else:
             print "what?", node.tag
-                
         return outstr    
     
     def nodepress(self,instance,value):
         print "press , ", value
         node = self.rootnode.xpath("//node[@ID='" + value + "']")[0]
-        print node
+        #print node
         if (node.get("FOLDED")=="False"):
             node.set("FOLDED","True")
         else:
@@ -56,14 +56,13 @@ class MainView(BoxLayout):
         
         self.raw_label_text = self.parse_node_to_text(self.rootnode.find("node"),0,"")
         self.mml.text=self.raw_label_text   
-        print self.raw_label_text
+        self.mml.texture_update()
     
     
     def filllabel(self):
         print "bla"
-        self.mml.text = self.read_map_from_file("testtemp.mm")
+        self.mml.text = self.read_map_from_file("test3.mm")
         self.mml.bind(on_ref_press=self.nodepress)
- #       self.mml.on_ref_press(self.nodepress)
         self.mml.markup=True
 
 class mmviewApp(App):
