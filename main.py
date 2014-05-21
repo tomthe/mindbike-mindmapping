a30c5a50e2fdb488525460b9548c6b1c78962d77
Tom Theile <tomthe@users.noreply.github.com>
2014-05-21 19:38:54 +0200

    positionierung in der mitte der kinderknoten funktioniert richtig! todo:  * scrollen in x-richtung  * verbindungslinien malen  * knotentext editieren  * mindmap speichern  * Datei-laden Dialog,  * UI...sch?neres design  * notizen anzeigen...  *

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import kivy
 from kivy.uix.modalview import ModalView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.scrollview import ScrollView
-from kivy.uix.widget import Widget
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ListProperty, StringProperty,ObjectProperty
 from kivy.app import App
@@ -19,17 +19,16 @@ class Node(Label):
     folded = False
     bbox=[100,15]
     nid=""
-    childnodes = ListProperty([])
+    #childnodes = ListProperty([])
     nodetextlabel = ObjectProperty()
     xmlnode = None
     bgcolor = ListProperty([.25,0.25,0.25])
-    
+
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
             if touch.is_double_tap:
                 print "double!!! ", self.text, self.pos,self.size
             else:
-                
                 if self.folded==False:
                     print "single-fold! ", self.text, self.pos,self.size
                     self.fold()
@@ -37,18 +36,18 @@ class Node(Label):
                     print "single-de-fold!! unfold! ", self.text, self.pos,self.size
                     self.unfold()
                 self.rootwidget.rebuild_map()
-            
+
     def fold(self):
         #print "fold:  ", self.text
         self.folded = True
         self.xmlnode.set("FOLDED","True")
-    
+
     def unfold(self):
         #print "unfold, "
         self.folded = False
         self.xmlnode.set("FOLDED","False")
         self.create_itself(self.xmlnode,self.rootwidget,[self.x,self.y],unfold=True)
-        
+
     def create_itself(self,xmlnode,rootwidget,pos,unfold=False):
         if xmlnode.tag=="node":
             self.xmlnode = xmlnode
@@ -56,9 +55,8 @@ class Node(Label):
             self.text = xmlnode.get("TEXT")
             self.texture_update()
             self.size = self.texture_size
-            self.bbox = [self.size[0],self.size[1]]
-            self.pos=[pos[0],pos[1]]
-                    
+            self.bby = self.height
+            self.pos = pos
             childpos=[pos[0]+self.size[0]+30,pos[1]]
             has_open_children = False
             if xmlnode.get("FOLDED",default="False")=="False" or unfold==True:
@@ -67,54 +65,54 @@ class Node(Label):
                 for nodechild in xmlnode:
                     if nodechild.tag=="node":
                         has_open_children=True
-                        #print "childnode... ", childpos, self.pos, "--",self.size
+
                         newnode = Node()
-                        #childpos=(childpos[0],childpos[1])
                         childboxy = newnode.create_itself(nodechild,rootwidget,childpos)
                         rootwidget.add_widget(newnode)
-                        #self.add_widget(newnode)
-                        self.childnodes.append(newnode)
+                        #self.childnodes.append(newnode)
                         childpos[1]  += childboxy
-                        self.bbox[1] += childboxy
+                        self.bby += childboxy
                 if has_open_children:
-                    
+                    pass
                     #print "has open children, ", self.text
-                    self.bbox[1] -=childboxy
-                    
+                    self.bby -= self.height
+                    #self.xmlnnode
+                    self.pos[1] += self.bby/2 - self.height/2
+
             else:
                 self.folded = True
                 #print "folded: ",self.text,xmlnode.find("node")
                 #print(etree.tostring(self.xmlnode, pretty_print=True))
                 if xmlnode.find("node") !=None:
-                    print " has folded kids", self.text
+                    #print " has folded kids", self.text
                     self.bgcolor = [.4,0.4,0.4]
                     self.canvas.ask_update()
             #self.size = [self.nwidth,self.nheight]
-            
-            return self.bbox[1]
+
+            return self.bby
         else:
             return 0
-            
-            
+
+
 class MainView(FloatLayout):
     rootnode=None
     firstnode=None
-    
+
     def read_map_from_file(self,filename):
         tree = etree.parse(filename)
         self.rootnode = tree.getroot()
         self.firstnode = self.rootnode.find("node")
         self.firstnode.set("FOLDED","False")
         self.build_map(self.firstnode,0,"")
-        
+
     def rebuild_map(self):
         self.clear_widgets()
-        firstnodepos = [0,0] 
+        firstnodepos = [0,0]
         newnode=Node()
         self.height = newnode.create_itself(self.firstnode,self,firstnodepos)
         self.add_widget(newnode)
-        
-    def build_map(self,node,level,outstr):    
+
+    def build_map(self,node,level,outstr):
         if node.tag=="node":
             newnode=Node()
             firstnodepos = [0,0]
@@ -122,9 +120,9 @@ class MainView(FloatLayout):
             self.add_widget(newnode)
         else:
             print "what?", node.tag
-        return outstr    
+        return outstr
 
-        
+
 class mmviewApp(App):
 
     def build(self):
@@ -132,7 +130,7 @@ class mmviewApp(App):
         mv.read_map_from_file("test3.mm")
         sv = ScrollView()
         sv.add_widget(mv)
-        
+
         return sv
 
 if __name__ == '__main__':
--- readmm.py
+++ readmm.py
@@ -9,10 +9,8 @@ Created on Sat May 17 02:02:43 2014
 
 from lxml import etree
 
-
-
 def printnode(node,level,outstr):
-    
+
     if node.tag=="node":
         #print " " * level, level, node.get("TEXT"), node.tag
         outstr += "    " * level + "[ref=" + node.get("ID") + "]" +  node.get("TEXT") +"[/ref]"+ chr(10)
@@ -22,21 +20,21 @@ def printnode(node,level,outstr):
                 outstr = printnode(nodechild,level+1,outstr)
     else:
         print "what?", node.tag
-            
+
     return outstr
-        
+
 def stringize(filename):
-    
+
     outstr=""
-    
+
     tree = etree.parse(filename)
     root = tree.getroot()
     print"blabla"
-    
+
     for child in root:
         if child.tag=="node":
             outstr = printnode(child,0,outstr)
-    
+
     print outstr
     return outstr
 
