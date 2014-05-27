94941ba2293ec99a7c729e9ccbf25742723b48be
Tom Theile <tomthe@users.noreply.github.com>
2014-05-27 16:42:33 +0200

    Save File, cElementTree, different windows machine  * save file  * tested on different windows machine  * support for cElementTree and ElementTree - no lxml necessary  * CREATED-time string is now produced correctly (?)

---
+++ test.mm
@@ -0,0 +1,120 @@
+<map version="freeplane 1.2.0">
+
+<node CREATED="1283093380553" FOLDED="False" ID="ID_1723255651" MODIFIED="1400192931075" TEXT="vk"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" show_note_icons="true" />
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="default" MAX_WIDTH="600" STYLE="as_parent">
+<font BOLD="false" ITALIC="false" NAME="SansSerif" SIZE="10" />
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" />
+<stylenode LOCALIZED_TEXT="defaultstyle.note" />
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge" />
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT" />
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode COLOR="#18898b" LOCALIZED_TEXT="styles.topic" STYLE="fork">
+<font BOLD="true" NAME="Liberation Sans" SIZE="10" />
+</stylenode>
+<stylenode COLOR="#cc3300" LOCALIZED_TEXT="styles.subtopic" STYLE="fork">
+<font BOLD="true" NAME="Liberation Sans" SIZE="10" />
+</stylenode>
+<stylenode COLOR="#669900" LOCALIZED_TEXT="styles.subsubtopic">
+<font BOLD="true" NAME="Liberation Sans" SIZE="10" />
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes" />
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="AutomaticLayout.level.root">
+<font SIZE="18" />
+</stylenode>
+<stylenode COLOR="#0033ff" LOCALIZED_TEXT="AutomaticLayout.level,1">
+<font SIZE="16" />
+</stylenode>
+<stylenode COLOR="#00b439" LOCALIZED_TEXT="AutomaticLayout.level,2">
+<font SIZE="14" />
+</stylenode>
+<stylenode COLOR="#990000" LOCALIZED_TEXT="AutomaticLayout.level,3">
+<font SIZE="12" />
+</stylenode>
+<stylenode COLOR="#111111" LOCALIZED_TEXT="AutomaticLayout.level,4">
+<font SIZE="10" />
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook COUNTER="3" NAME="AutomaticEdgeColor" />
+<node CREATED="1400192757998" FOLDED="False" ID="ID_174910959" MODIFIED="1400192766003" POSITION="right" TEXT="svsd">
+<node CREATED="1.4012009712e+12" FOLDED="False" ID="ID_6201543845" MODIFIED="1.4012009712e+12" TEXT="bubububu">
+<node CREATED="1.40120097785e+12" ID="ID_8529991111" MODIFIED="1.40120097785e+12" TEXT="_" /></node><edge COLOR="#ff0000" />
+<node CREATED="1401200483245" ID="ID_7266474466" MODIFIED="1401200483245" TEXT="neuer knoten!" />
+<node CREATED="1400192767274" FOLDED="False" ID="ID_306695723" MODIFIED="1400197606528" TEXT="knot1-1">
+<node CREATED="1401201114481" FOLDED="False" ID="ID_9009993216" MODIFIED="1401201114481" TEXT="_oooooooooooooooooooooooooooooooooooo">
+<node CREATED="1401201116707" ID="ID_9686899139" MODIFIED="1401201116707" TEXT="_" /></node><node CREATED="1401201108282" ID="ID_4608703919" MODIFIED="1401201108282" TEXT="kaaaaaaaaaaaaaaaa" /><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      das ist eine notiz
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1400192767274" ID="ID_306695726" MODIFIED="1400192770909" TEXT="knot1-565h" />
+<node CREATED="1400192771540" ID="ID_1021949625" MODIFIED="1400197616894" TEXT="knot1-2"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      detail uh
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1400197618291" ID="ID_796364143" MODIFIED="1400197644346" TEXT="mit attribut + &#252;">
+<attribute NAME="atr1" VALUE="55b" />
+<node CREATED="1401125723672" ID="ID_1711889197" MODIFIED="1401125723672" TEXT="ccs" />
+</node>
+</node>
+<node CREATED="1400286744535" FOLDED="False" ID="ID_301477270" MODIFIED="1400286748197" POSITION="right" TEXT="knoten2&#252;k&#252;k&#252;k&#252;">
+<edge COLOR="#0000ff" />
+<node CREATED="1400286748199" ID="ID_1644751104" MODIFIED="1400286754921" TEXT="kno 2-2" />
+<node CREATED="1400286755333" FOLDED="False" ID="ID_1241684129" MODIFIED="1400286760843" TEXT="kno2-3">
+<node CREATED="1400286763965" FOLDED="False" ID="ID_1876155295" MODIFIED="1400286771485" TEXT="kno2-3-1">
+<node CREATED="1400286772116" ID="ID_1851170645" MODIFIED="1400286776775" TEXT="tochtertothcher" />
+<node CREATED="1401200702864" ID="ID_712632725" MODIFIED="1401200706867" TEXT="tochterschwester" />
+</node>
+</node>
+</node>
+<node CREATED="1400627607275" ID="ID_1558588684" MODIFIED="1400627626902" POSITION="right" TEXT="bubububu">
+<edge COLOR="#0000ff" />
+<node CREATED="1400627610710" ID="ID_1775235660" MODIFIED="1400627614648" TEXT="bibi" />
+<node CREATED="1400627615468" ID="ID_983148937" MODIFIED="1400627617497" TEXT="baba" />
+<node CREATED="1400627618501" ID="ID_1274210085" MODIFIED="1400627621188" TEXT="biba" />
+</node>
+<node CREATED="1400627629823" ID="ID_1546809166" MODIFIED="1400627635110" POSITION="right" TEXT="numaaamama">
+<edge COLOR="#00ff00" />
+<node CREATED="1400627635126" ID="ID_1004240095" MODIFIED="1400627638635" TEXT="nimi" />
+<node CREATED="1400627639157" ID="ID_639439588" MODIFIED="1400627641013" TEXT="mama" />
+<node CREATED="1400627642237" ID="ID_1066062050" MODIFIED="1400627644431" TEXT="mima" />
+<node CREATED="1400631880970" ID="ID_101238715" MODIFIED="1400631884746" TEXT="mimimimi">
+<node CREATED="1400631884748" ID="ID_58721238" MODIFIED="1400631890628" TEXT="mimimimimiminana" />
+</node>
+</node>
+</node>
+</map>
