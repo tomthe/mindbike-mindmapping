d170c7395f248433b8bed520ca36a74c3571ce22
Tom Theile <tomthe@users.noreply.github.com>
2014-06-01 12:08:15 +0200

    zwischenstand

---
+++ main.py
@@ -0,0 +1,318 @@
+# -*- coding: utf-8 -*-
+
+# MindBike Mindmapping - Mindmap viewer and editor for mobile devices and desktop platforms
+# Copyright (C) 2014 Tom Theile
+# This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+import kivy
+from kivy.uix.modalview import ModalView
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ListProperty, StringProperty,ObjectProperty, BooleanProperty
+from kivy.uix.popup import Popup
+from kivy.app import App
+from random import randint
+import time
+#from readmm import stringize
+try:
+  from lxml import etree
+  print("running with lxml.etree")
+except ImportError:
+  try:
+    # Python 2.5
+    import xml.etree.cElementTree as etree
+    print("running with cElementTree on Python 2.5+")
+  except ImportError:
+    try:
+      # Python 2.5
+      import xml.etree.ElementTree as etree
+      print("running with ElementTree on Python 2.5+")
+    except ImportError:
+      try:
+        # normal cElementTree install
+        import cElementTree as etree
+        print("running with cElementTree")
+      except ImportError:
+        try:
+          # normal ElementTree install
+          import elementtree.ElementTree as etree
+          print("running with ElementTree")
+        except ImportError:
+          print("Failed to import ElementTree from any known place")
+
+
+kivy.require('1.0.7')
+
+class NodeTextInput(TextInput):
+    node=None
+
+    def __init__(self,**kwargs):
+        #print "node kwargs:", kwargs
+        self.node=kwargs['node']
+        #self.bind(focus=self.on_focus)
+        super(NodeTextInput, self).__init__(**kwargs)
+
+    def on_text_validate(self):
+        newtext=self.text
+        self.node.text=unicode(newtext,'utf-8')
+        self.node.on_text2()
+        self.node.remove_widget(self)
+
+    def on_focus(self,instance,value):
+        if value:
+            pass
+            #print "focus",instance, ",-, ", value
+        else:
+            #print "defocus",instance, ",-, ", value
+            self.on_text_validate()
+
+        return super(NodeTextInput, self).on_focus(instance,value)
+
+    def on_enter(self,instance,value):
+        pass
+        # print "enter gedrueckt!!"
+
+
+class Node(Label):
+    selected=BooleanProperty(False)
+    ntext = StringProperty()
+    folded = False
+    bbox=[100,15]
+    nid=""
+    #childnodes = ListProperty([])
+    nodetextlabel = ObjectProperty()
+    xmlnode = None
+    bgcolor = ListProperty([.25,0.25,0.25])
+    #posright = AliasProperty(get_posright, set_posright, bind=('pos', 'width'))
+    fathers_end_pos=[0,33]
+    fathers_width=20
+
+    def on_selected(self,instance,value):
+        #self.rootwidget.deselect_all()
+        if value:
+            self.bgcolor = [0.3,0.1,0.1]
+        else:
+            self.bgcolor = [0.25,0.25,0.25]
+
+    def set_posright(self,value):
+        self.pos= [value[0] - self.width, value[1]]
+
+    def get_posright(self):
+        return [self.x + self.width,self.y]
+
+    def on_touch_down(self, touch):
+        self.selected =False
+        if self.collide_point(*touch.pos):
+            if touch.is_double_tap:
+                print "double!!! ", self.text, self.pos,self.size
+                self.edit()
+            else:
+                #self.fold_unfold()
+                self.selected = not self.selected
+        return super(Node, self).on_touch_down(touch)
+
+    def edit(self):
+        inputsize = self.size[0]+66, self.size[1]+24
+        textinput = NodeTextInput(node=self,size=inputsize, pos=self.pos,text=self.text, focus=True, multiline=False)
+        self.add_widget(textinput,-1)
+
+
+    def fold_unfold(self):
+        #print "fold,unfold"
+        if self.folded==False:
+            print "single-fold! ", self.text, self.pos,self.size
+            self.fold()
+        else:
+            print "single-de-fold!! unfold! ", self.text, self.pos,self.size
+            self.unfold()
+        self.rootwidget.rebuild_map()
+
+    def fold(self):
+        #print "fold:  ", self.text
+        self.folded = True
+        self.xmlnode.set("FOLDED","True")
+
+    def unfold(self):
+        #print "unfold, "
+        self.folded = False
+        self.xmlnode.set("FOLDED","False")
+        self.create_itself(self.xmlnode,self.rootwidget,[self.x,self.y],unfold=True)
+
+    def on_text2(self):
+        #print "on_text",unicode(self.text)#,instance,value
+        #self.width=self.texture_size[0]
+        self.xmlnode.set(u"TEXT",unicode(self.text,))
+        self.rootwidget.rebuild_map()
+
+    def add_child(self):
+        #einen neuen "node" erstellen
+        #<node TEXT="knot1-2" ID="ID_1021949625" CREATED="1400192771540" MODIFIED="1400197616894">
+        #TEXT="nimi" ID="ID_1004240095" CREATED="140 062 763 512 6" MODIFIED="1400627638635"
+
+        newxmlnode = etree.Element("node")
+        #The Text is empty
+        TEXT="_"
+        #a random ID between 10E9 and 10E10:
+        ID = "ID_"+ str(randint(1000000000,10000000000))
+        #Seconds since epoch:
+        CREATED = str(int(time.time()*1000))
+
+        #set the xml-node-Attributes:
+        newxmlnode.set("TEXT",TEXT)
+        newxmlnode.set("ID",ID)
+        newxmlnode.set("CREATED",CREATED)
+        newxmlnode.set("MODIFIED",CREATED)
+        self.xmlnode.set("FOLDED","False")
+        self.xmlnode.insert(0,newxmlnode)
+        self.rootwidget.rebuild_map()
+
+    def create_itself(self,xmlnode,rootwidget,pos,unfold=False,fathers_end_pos=[20,20],fathers_width=50):
+        if xmlnode.tag=="node":
+            #print "fathersendpos.pos, self.ext: (nnerhalb...,", self.fathers_end_pos,self.text, "fathers..."
+            self.xmlnode = xmlnode
+            self.rootwidget = rootwidget
+            self.fathers_end_pos= fathers_end_pos
+            self.fathers_width = fathers_width
+            self.text = xmlnode.get("TEXT")
+            self.texture_update()
+            self.size = self.texture_size
+            self.bby = self.height+4
+            self.pos = pos
+            childpos=[pos[0]+self.size[0]+70,pos[1]]
+            has_open_children = False
+            if xmlnode.get("FOLDED")=="False" or unfold==True:
+                xmlnode.set("FOLDED","False")
+                self.folded=False
+                for nodechild in xmlnode:
+                    if nodechild.tag=="node":
+                        has_open_children=True
+
+                        newnode = Node()
+                        #print "self.pos, self.ext: (nnerhalb...,",self.pos,self.text
+                        # newnode.canvas.
+                        childboxy = newnode.create_itself(nodechild,rootwidget,childpos,fathers_end_pos= self.pos,fathers_width=self.width)
+                        rootwidget.add_widget(newnode)
+                        #self.childnodes.append(newnode)
+                        childpos[1]  += childboxy
+                        self.bby += childboxy
+
+                if has_open_children:
+                    pass
+                    #print "has open children, ", self.text
+                    self.bby -= self.height
+                    #self.xmlnnode
+                    self.pos[1] += self.bby/2 - self.height/2 -4
+
+            else:
+                self.folded = True
+                #print "folded: ",self.text,xmlnode.find("node")
+                #print(etree.tostring(self.xmlnode, pretty_print=True))
+                if xmlnode.find("node") !=None:
+                    #print " has folded kids", self.text
+                    self.bgcolor = [.4,0.4,0.4]
+                    self.canvas.ask_update()
+            #self.size = [self.nwidth,self.nheight]
+
+            if self.right > self.rootwidget.width:
+                self.rootwidget.width = self.right + 50
+            return self.bby
+        else:
+            return 0
+
+
+class MapView(FloatLayout):
+    rootnode=None
+    firstnode=None
+    loaded_map_filename = "test.mm"
+
+    def read_map_from_file(self,filename):
+        self.loaded_map_filename = filename
+        print "parse:", filename
+        self.tree = etree.parse(filename)
+        print "parsed..."
+        self.rootnode = self.tree.getroot()
+        self.firstnode = self.rootnode.find("node")
+        self.firstnode.set("FOLDED","False")
+        print "build_map..."
+        self.build_map(self.firstnode,0,"")
+
+    def rebuild_map(self):
+        self.clear_widgets()
+        firstnodepos = [0,0]
+        newnode=Node()
+        self.height = newnode.create_itself(self.firstnode,self,firstnodepos,fathers_end_pos=[0,self.height/2])
+        self.add_widget(newnode)
+
+    def build_map(self,node,level,outstr):
+        if node.tag=="node":
+            newnode=Node(fathers_end_pos=[20,0])
+            firstnodepos = [0,0]
+            self.height = newnode.create_itself(node,self,firstnodepos)
+            self.add_widget(newnode)
+        else:
+            print "what?", node.tag
+        return outstr
+
+    def deselect_all(self):
+        for node in self.children:
+            node.selected = False
+
+    def save_map_to_file(self,filename):
+        if filename==None:
+            filename = self.loaded_map_filename
+        self.tree.write(filename)
+        print "map saved to: ", filename
+
+    def close_map(self):
+        self.clear_widgets()
+        self.rootnode=None
+        self.firstnode=None
+
+class MindmapApp(FloatLayout):
+    mv=ObjectProperty()
+
+    def load_map(self,filename):
+        try:
+            print "close map"
+            self.mv.close_map()
+            print "open map from file: ", filename
+            self.mv.read_map_from_file(filename)
+        except:
+            print "oh, loading ",filename, " didn't work... try again?"
+            self.dismiss_popup()
+
+
+    def dismiss_popup(self):
+        try:
+            self._popup.dismiss()
+        except:
+            pass
+
+    def show_load(self):
+        content = LoadDialog(load=self.load_map, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def save_map(self):
+        self.mv.save_map_to_file()
+
+
+class LoadDialog(ModalView):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+class mmviewApp(App):
+
+    def build(self):
+        mindmapapp=MindmapApp()
+        mindmapapp.load_map("test.mm")
+        return mindmapapp
+
+if __name__ == '__main__':
+    mmviewApp().run()
---
+++ testtemp.mm
@@ -0,0 +1,115 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="vk" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1400192931075"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<node TEXT="tNONONDn1" POSITION="right" ID="ID_174910959" CREATED="1400192757998" MODIFIED="1400192766003">
+<edge COLOR="#ff0000"/>
+<node TEXT="knot1-1" ID="ID_306695723" CREATED="1400192767274" MODIFIED="1400197606528"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      das ist eine notiz
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="knot1-565h" ID="ID_306695726" CREATED="1400192767274" MODIFIED="1400192770909"/>
+<node TEXT="knot1-2" ID="ID_1021949625" CREATED="1400192771540" MODIFIED="1400197616894"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      detail uh
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="mit attribut + &#xfc;" ID="ID_796364143" CREATED="1400197618291" MODIFIED="1400197644346">
+<attribute NAME="atr1" VALUE="55b"/>
+<node TEXT="ccs" ID="ID_1711889197" CREATED="1401125723672" MODIFIED="1401125723672"/>
+</node>
+</node>
+<node TEXT="knoten2&#xfc;k&#xfc;k&#xfc;k&#xfc;" POSITION="right" ID="ID_301477270" CREATED="1400286744535" MODIFIED="1400286748197">
+<edge COLOR="#0000ff"/>
+<node TEXT="kno 2-2" ID="ID_1644751104" CREATED="1400286748199" MODIFIED="1400286754921"/>
+<node TEXT="kno2-3" ID="ID_1241684129" CREATED="1400286755333" MODIFIED="1400286760843">
+<node TEXT="kno2-3-1" ID="ID_1876155295" CREATED="1400286763965" MODIFIED="1400286771485">
+<node TEXT="tochtertothcher" ID="ID_1851170645" CREATED="1400286772116" MODIFIED="1400286776775"/>
+</node>
+</node>
+</node>
+<node TEXT="bubububu" POSITION="right" ID="ID_1558588684" CREATED="1400627607275" MODIFIED="1400627626902">
+<edge COLOR="#0000ff"/>
+<node TEXT="bibi" ID="ID_1775235660" CREATED="1400627610710" MODIFIED="1400627614648"/>
+<node TEXT="baba" ID="ID_983148937" CREATED="1400627615468" MODIFIED="1400627617497"/>
+<node TEXT="biba" ID="ID_1274210085" CREATED="1400627618501" MODIFIED="1400627621188"/>
+</node>
+<node TEXT="numaaamama" POSITION="right" ID="ID_1546809166" CREATED="1400627629823" MODIFIED="1400627635110">
+<edge COLOR="#00ff00"/>
+<node TEXT="nimi" ID="ID_1004240095" CREATED="1400627635126" MODIFIED="1400627638635"/>
+<node TEXT="mama" ID="ID_639439588" CREATED="1400627639157" MODIFIED="1400627641013"/>
+<node TEXT="mima" ID="ID_1066062050" CREATED="1400627642237" MODIFIED="1400627644431"/>
+<node TEXT="mimimimi" ID="ID_101238715" CREATED="1400631880970" MODIFIED="1400631884746">
+<node TEXT="mimimimimiminana" ID="ID_58721238" CREATED="1400631884748" MODIFIED="1400631890628"/>
+</node>
+</node>
+</node>
+</map>
