0c7c8a851332f2c49e65c75d97e2a526b543f27a
thomthe <tomi2@web.de>
2014-06-20 15:09:59 +0200

    save family-connections in every node-widget. easier selection of siblings, fathers...
    todo:
    add_child - auto select this child

--- test.mm
+++ test.mm
@@ -1,64 +1,64 @@
 <map version="freeplane 1.3.0">
-<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+
 <history ID="44">history!!</history>
-<node TEXT="wurzel" FOLDED="False" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1400192931075"><hook NAME="MapStyle">
-    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+<node CREATED="1283093380553" FOLDED="False" ID="ID_1723255651" MODIFIED="1400192931075" TEXT="wurzel"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" show_note_icons="true" />
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node">
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
-<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="600">
-<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<stylenode COLOR="#000000" LOCALIZED_TEXT="default" MAX_WIDTH="600" STYLE="as_parent">
+<font BOLD="false" ITALIC="false" NAME="SansSerif" SIZE="10" />
 </stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
-<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" />
+<stylenode LOCALIZED_TEXT="defaultstyle.note" />
 <stylenode LOCALIZED_TEXT="defaultstyle.floating">
-<edge STYLE="hide_edge"/>
-<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<edge STYLE="hide_edge" />
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT" />
 </stylenode>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
-<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
-<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<stylenode COLOR="#18898b" LOCALIZED_TEXT="styles.topic" STYLE="fork">
+<font BOLD="true" NAME="Liberation Sans" SIZE="10" />
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
-<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<stylenode COLOR="#cc3300" LOCALIZED_TEXT="styles.subtopic" STYLE="fork">
+<font BOLD="true" NAME="Liberation Sans" SIZE="10" />
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
-<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+<stylenode COLOR="#669900" LOCALIZED_TEXT="styles.subsubtopic">
+<font BOLD="true" NAME="Liberation Sans" SIZE="10" />
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.important">
-<icon BUILTIN="yes"/>
+<icon BUILTIN="yes" />
 </stylenode>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
-<font SIZE="18"/>
+<stylenode COLOR="#000000" LOCALIZED_TEXT="AutomaticLayout.level.root">
+<font SIZE="18" />
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
-<font SIZE="16"/>
+<stylenode COLOR="#0033ff" LOCALIZED_TEXT="AutomaticLayout.level,1">
+<font SIZE="16" />
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
-<font SIZE="14"/>
+<stylenode COLOR="#00b439" LOCALIZED_TEXT="AutomaticLayout.level,2">
+<font SIZE="14" />
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
-<font SIZE="12"/>
+<stylenode COLOR="#990000" LOCALIZED_TEXT="AutomaticLayout.level,3">
+<font SIZE="12" />
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
-<font SIZE="10"/>
+<stylenode COLOR="#111111" LOCALIZED_TEXT="AutomaticLayout.level,4">
+<font SIZE="10" />
 </stylenode>
 </stylenode>
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
-<node TEXT="svsd" POSITION="right" ID="ID_174910959" CREATED="1400192757998" MODIFIED="1400192766003">
-<edge COLOR="#ff0000"/>
-<node TEXT="bubububu" ID="ID_6201543845" CREATED="1401206854780" MODIFIED="1401206854780">
-<node TEXT="_" ID="ID_8529991111" CREATED="1401206854781" MODIFIED="1401206854781"/>
+<hook COUNTER="3" NAME="AutomaticEdgeColor" />
+<node CREATED="1400192757998" ID="ID_174910959" MODIFIED="1400192766003" POSITION="right" TEXT="svsd">
+<edge COLOR="#ff0000" />
+<node CREATED="1401206854780" ID="ID_6201543845" MODIFIED="1401206854780" TEXT="bubububu">
+<node CREATED="1401206854781" ID="ID_8529991111" MODIFIED="1401206854781" TEXT="_" />
 </node>
-<node TEXT="neuer knoten!" ID="ID_7266474466" CREATED="1401200483245" MODIFIED="1401200483245"/>
-<node TEXT="knot1-1" ID="ID_306695723" CREATED="1400192767274" MODIFIED="1400197606528"><richcontent TYPE="NOTE">
+<node CREATED="1401200483245" ID="ID_7266474466" MODIFIED="1401200483245" TEXT="neuer knoten!" />
+<node CREATED="1400192767274" ID="ID_306695723" MODIFIED="1400197606528" TEXT="knot1-1"><richcontent TYPE="NOTE">
 
 <html>
   <head>
@@ -71,13 +71,13 @@
   </body>
 </html>
 </richcontent>
-<node TEXT="_oooooooooooooooooooooooooooooooooooo" ID="ID_9009993216" CREATED="1401201114481" MODIFIED="1401201114481">
-<node TEXT="_" ID="ID_9686899139" CREATED="1401201116707" MODIFIED="1401201116707"/>
+<node CREATED="1401201114481" ID="ID_9009993216" MODIFIED="1401201114481" TEXT="_oooooooooooooooooooooooooooooooooooo">
+<node CREATED="1401201116707" ID="ID_9686899139" MODIFIED="1401201116707" TEXT="_" />
 </node>
-<node TEXT="kaaaaaaaaaaaaaaaa" ID="ID_4608703919" CREATED="1401201108282" MODIFIED="1401201108282"/>
+<node CREATED="1401201108282" ID="ID_4608703919" MODIFIED="1401201108282" TEXT="kaaaaaaaaaaaaaaaa" />
 </node>
-<node TEXT="knot1-565h" ID="ID_306695726" CREATED="1400192767274" MODIFIED="1400192770909"/>
-<node TEXT="knot1-2" ID="ID_1021949625" CREATED="1400192771540" MODIFIED="1400197616894"><richcontent TYPE="DETAILS">
+<node CREATED="1400192767274" ID="ID_306695726" MODIFIED="1400192770909" TEXT="knot1-565h" />
+<node CREATED="1400192771540" ID="ID_1021949625" MODIFIED="1400197616894" TEXT="knot1-2"><richcontent TYPE="DETAILS">
 
 <html>
   <head>
@@ -91,35 +91,35 @@
 </html>
 </richcontent>
 </node>
-<node TEXT="mit attribut + &#252;" ID="ID_796364143" CREATED="1400197618291" MODIFIED="1400197644346">
-<attribute NAME="atr1" VALUE="55b"/>
-<node TEXT="ccs" ID="ID_1711889197" CREATED="1401125723672" MODIFIED="1401125723672"/>
-<node TEXT="ninini" ID="ID_339133848" CREATED="1401206860551" MODIFIED="1401206863076"/>
+<node CREATED="1400197618291" ID="ID_796364143" MODIFIED="1400197644346" TEXT="mit attribut + &#252;">
+<attribute NAME="atr1" VALUE="55b" />
+<node CREATED="1401125723672" ID="ID_1711889197" MODIFIED="1401125723672" TEXT="ccs" />
+<node CREATED="1401206860551" ID="ID_339133848" MODIFIED="1401206863076" TEXT="ninini" />
 </node>
 </node>
-<node TEXT="knoten2&#252;k&#252;k&#252;k&#252;" POSITION="right" ID="ID_301477270" CREATED="1400286744535" MODIFIED="1400286748197" FOLDED="False">
-<edge COLOR="#0000ff"/>
-<node TEXT="kno 2-2" ID="ID_1644751104" CREATED="1400286748199" MODIFIED="1400286754921"/>
-<node TEXT="kno2-3" ID="ID_1241684129" CREATED="1400286755333" MODIFIED="1400286760843">
-<node TEXT="kno2-3-1" ID="ID_1876155295" CREATED="1400286763965" MODIFIED="1400286771485">
-<node TEXT="tochtertothcher" ID="ID_1851170645" CREATED="1400286772116" MODIFIED="1400286776775"/>
-<node TEXT="tochterschwester" ID="ID_712632725" CREATED="1401200702864" MODIFIED="1401200706867"/>
+<node CREATED="1400286744535" FOLDED="False" ID="ID_301477270" MODIFIED="1400286748197" POSITION="right" TEXT="knoten2&#252;k&#252;k&#252;k&#252;">
+<edge COLOR="#0000ff" />
+<node CREATED="1400286748199" ID="ID_1644751104" MODIFIED="1400286754921" TEXT="kno 2-2" />
+<node CREATED="1400286755333" FOLDED="False" ID="ID_1241684129" MODIFIED="1400286760843" TEXT="kno2-3">
+<node CREATED="1400286763965" FOLDED="False" ID="ID_1876155295" MODIFIED="1400286771485" TEXT="kno2-3-1">
+<node CREATED="1400286772116" ID="ID_1851170645" MODIFIED="1400286776775" TEXT="tochtertothcher" />
+<node CREATED="1401200702864" ID="ID_712632725" MODIFIED="1401200706867" TEXT="tochterschwesterggg" />
 </node>
 </node>
 </node>
-<node TEXT="bubububu" POSITION="right" ID="ID_1558588684" CREATED="1400627607275" MODIFIED="1400627626902">
-<edge COLOR="#0000ff"/>
-<node TEXT="bibi" ID="ID_1775235660" CREATED="1400627610710" MODIFIED="1400627614648"/>
-<node TEXT="baba" ID="ID_983148937" CREATED="1400627615468" MODIFIED="1400627617497"/>
-<node TEXT="biba" ID="ID_1274210085" CREATED="1400627618501" MODIFIED="1400627621188"/>
+<node CREATED="1400627607275" ID="ID_1558588684" MODIFIED="1400627626902" POSITION="right" TEXT="bubububu">
+<edge COLOR="#0000ff" />
+<node CREATED="1400627610710" ID="ID_1775235660" MODIFIED="1400627614648" TEXT="bibi" />
+<node CREATED="1400627615468" ID="ID_983148937" MODIFIED="1400627617497" TEXT="baba" />
+<node CREATED="1400627618501" ID="ID_1274210085" MODIFIED="1400627621188" TEXT="biba" />
 </node>
-<node TEXT="numaaamama" POSITION="right" ID="ID_1546809166" CREATED="1400627629823" MODIFIED="1400627635110">
-<edge COLOR="#00ff00"/>
-<node TEXT="nimi" ID="ID_1004240095" CREATED="1400627635126" MODIFIED="1400627638635"/>
-<node TEXT="mama" ID="ID_639439588" CREATED="1400627639157" MODIFIED="1400627641013"/>
-<node TEXT="mima" ID="ID_1066062050" CREATED="1400627642237" MODIFIED="1400627644431"/>
-<node TEXT="mimimimi" ID="ID_101238715" CREATED="1400631880970" MODIFIED="1400631884746">
-<node TEXT="mimimimimiminana" ID="ID_58721238" CREATED="1400631884748" MODIFIED="1400631890628"/>
+<node CREATED="1400627629823" FOLDED="False" ID="ID_1546809166" MODIFIED="1400627635110" POSITION="right" TEXT="numaaamama">
+<edge COLOR="#00ff00" />
+<node CREATED="1400627635126" ID="ID_1004240095" MODIFIED="1400627638635" TEXT="nimi" />
+<node CREATED="1400627639157" ID="ID_639439588" MODIFIED="1400627641013" TEXT="mama" />
+<node CREATED="1400627642237" ID="ID_1066062050" MODIFIED="1400627644431" TEXT="mima" />
+<node CREATED="1400631880970" FOLDED="False" ID="ID_101238715" MODIFIED="1400631884746" TEXT="mimimimi">
+<node CREATED="1400631884748" ID="ID_58721238" MODIFIED="1400631890628" TEXT="mimimimimiminana" />
 </node>
 </node>
 </node>
