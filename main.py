7fa61d05c63a84fb7865d2c015bd4ea77195b985
thomthe <tomi2@web.de>
2014-08-05 15:11:43 +0200

    version count...

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.3"
+__version__ = "0.4.4"
 
 import kivy
 from kivy.app import App
